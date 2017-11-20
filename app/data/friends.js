0c302cdb88c89f829ec6d677a8a841528e4b64d1
Suha Park <suhapark@Suhas-MacBook-Pro.local>
2017-11-20 00:20:35 -0800

    add server

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//an array of objectss
+//starting with dummy data
+var friendList = [
+  {
+    "name": "Johnny Depp",
+    "photo": "https://i.pinimg.com/originals/30/81/bb/3081bb6cdf4e9a1bec4a8a5b75ba2d8e.jpg",
+    "scores": [ 5, 5, 4, 4, 5, 5, 5, 5, 4, 5]
+  },
+  {
+    "name": "Pug Woof",
+    "photo": "https://i.pinimg.com/originals/39/55/78/395578ce8c2319411106a0c8e2c2ed0f.jpg",
+    "scores": [ 3, 1, 2, 3, 3, 3, 3, 3, 3, 2]
+  },
+  {
+    "name": "Daisy Leaves",
+    "photo": "https://i.pinimg.com/736x/64/8e/ab/648eabd4e77c5809a509d4c7b9bfcf18--moon-moon-so-cute.jpg",
+    "scores": [ 1, 2, 1, 1, 2, 3, 2, 3, 1, 1]
+  },
+]
+
+module.exports = friendList;
